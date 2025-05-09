688d102875c2c3705d22118c937e117a963ee478
ZenildoNeto <130871331+ZenildoNeto@users.noreply.github.com>
2025-05-09 20:55:28 +0000

    versao inicial

---
+++ data.js
@@ -0,0 +1,82 @@
+const portfolioData = {
+    totalBalance: 150000.00,
+    categories: [
+        {
+            name: "Renda Fixa",
+            value: 75000.00,
+            change: 5.2,
+            color: "blue",
+            percentage: 50
+        },
+        {
+            name: "Renda Variável",
+            value: 45000.00,
+            change: -2.1,
+            color: "green",
+            percentage: 30
+        },
+        {
+            name: "Fundos Imobiliários",
+            value: 30000.00,
+            change: 3.7,
+            color: "yellow",
+            percentage: 20
+        }
+    ],
+    investments: [
+        {
+            name: "Tesouro IPCA+",
+            type: "Renda Fixa",
+            value: 25000.00,
+            change: 6.5
+        },
+        {
+            name: "CDB Banco Inter",
+            type: "Renda Fixa",
+            value: 20000.00,
+            change: 4.8
+        },
+        {
+            name: "LCI Caixa",
+            type: "Renda Fixa",
+            value: 30000.00,
+            change: 4.2
+        },
+        {
+            name: "PETR4",
+            type: "Ações",
+            value: 15000.00,
+            change: -3.2
+        },
+        {
+            name: "VALE3",
+            type: "Ações",
+            value: 18000.00,
+            change: 2.8
+        },
+        {
+            name: "ITUB4",
+            type: "Ações",
+            value: 12000.00,
+            change: -1.5
+        },
+        {
+            name: "HGLG11",
+            type: "FII",
+            value: 10000.00,
+            change: 4.1
+        },
+        {
+            name: "XPLG11",
+            type: "FII",
+            value: 12000.00,
+            change: 3.5
+        },
+        {
+            name: "MXRF11",
+            type: "FII",
+            value: 8000.00,
+            change: 2.9
+        }
+    ]
+};
